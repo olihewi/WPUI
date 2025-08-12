3502e879a7a3fba9636fa6e1c070aaa3bd384eec
Oli <hewisonoliver@gmail.com>
2025-08-13 00:33:27 +0100

    NCER parsing (CellsFile), OAM stuff

--- Assets/WPUI/Nitro/Files/CellsFile.cs
+++ Assets/WPUI/Nitro/Files/CellsFile.cs
@@ -1,14 +1,102 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
 using WPUI.Nitro.Attributes;
+using WPUI.Nitro.Structs;
+using WPUI.Nitro.Utils;
 
 namespace WPUI.Nitro.Files
 {
     [Magic("NCER")]
     public sealed class CellsFile : NitroFile
     {
+        [Magic("CEBK"), StructLayout(LayoutKind.Explicit)]
+        public struct CEBKBlock
+        {
+            [FieldOffset(0x00)] public ushort CellCount;
+            [FieldOffset(0x02)] public CellBankAttributes CellBankAttributes;
+            [FieldOffset(0x04)] public uint CellDataOffset;                      // Relative to start of CEBK
+            [FieldOffset(0x08)] public TilesFile.MappingMode MappingMode;
+            [FieldOffset(0x0C)] public uint VramTransferDataOffset;              // Relative to start of CEBK or 0
+            [FieldOffset(0x10)] public uint StringBankDataOffset;                // Relative to start of CEBK or 0
+            [FieldOffset(0x14)] public uint UserExtendedCellAttributeDataOffset; // Relative to start of CEBK or 0
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct Cell
+        {
+            // Required
+            [FieldOffset(0x00)] public ushort OamCount;
+            [FieldOffset(0x02)] public ushort CellAttributes;
+            [FieldOffset(0x04)] public uint OamDataOffset; // Relative to start of OAM data (after cell array)
+            // Optional, determined by CEBKBlock.CellBankAttributes
+            [FieldOffset(0x08)] public short xMax;
+            [FieldOffset(0x0A)] public short yMax;
+            [FieldOffset(0x0C)] public short xMin;
+            [FieldOffset(0x0E)] public short yMin;
+            //
+            public Oam[] Oams;
+        }
+
+
+        [Flags]
+        public enum CellBankAttributes : ushort
+        {
+            None = 0,
+            ContainsBoundingBoxes = 1,
+        }
+
+        public CEBKBlock cebkBlock;
+        public Cell[] Cells;
         public override void Read(BinaryReader br)
         {
-            throw new System.NotImplementedException();
+            ReadHeader(br);
+            for (int blockIdx = 0; blockIdx < header.BlockCount; blockIdx++)
+            {
+                long blockStart = br.BaseStream.Position;
+                string magic = ReadMagic(br);
+                int blockSize = (int)br.ReadUInt32();
+                switch (magic)
+                {
+                    case "KBEC":
+                    {
+                        cebkBlock = ReadStruct<CEBKBlock>(br);
+                        bool containsBoundingBoxes = (cebkBlock.CellBankAttributes & CellBankAttributes.ContainsBoundingBoxes) != 0;
+                        var cellsBase = blockStart + 8 + cebkBlock.CellDataOffset;
+                        var cellStride = containsBoundingBoxes ? 0x10 : 0x08;
+                        var oamsBase = cellsBase + cebkBlock.CellCount * cellStride;
+                        
+                        Cells = new Cell[cebkBlock.CellCount];
+                        for (int cellIdx = 0; cellIdx < cebkBlock.CellCount; cellIdx++)
+                        {
+                            br.BaseStream.Seek(cellsBase + cellIdx * cellStride, SeekOrigin.Current);
+                            var cell = new Cell()
+                            {
+                                OamCount = br.ReadUInt16(),
+                                CellAttributes = br.ReadUInt16(),
+                                OamDataOffset = br.ReadUInt32(),
+                            };
+                            if (containsBoundingBoxes)
+                            {
+                                cell.xMax = br.ReadInt16();
+                                cell.yMax = br.ReadInt16();
+                                cell.xMin = br.ReadInt16();
+                                cell.yMin = br.ReadInt16();
+                            }
+
+                            br.BaseStream.Seek(oamsBase + cell.OamDataOffset, SeekOrigin.Current);
+                            cell.Oams = new Oam[cell.OamCount];
+                            for (int oamIdx = 0; oamIdx < cell.OamCount; oamIdx++)
+                            {
+                                cell.Oams[oamIdx] = Oam.ReadOam(br);
+                            }
+                        }
+                        break;
+                    }
+                    // TODO LABL Block
+                }
+                br.BaseStream.Seek(blockStart + blockSize, SeekOrigin.Current);
+            }
         }
 
         public override void Write(BinaryWriter bw)
---
+++ Assets/WPUI/Nitro/Structs/Oam.cs
@@ -0,0 +1,141 @@
+﻿using System.IO;
+using System.Runtime.InteropServices;
+
+namespace WPUI.Nitro.Structs
+{
+    public enum OamShape : byte
+    {
+        Square = 0,
+        Horizontal = 1,
+        Vertical = 2,
+    }
+
+    public enum OamMode : byte
+    {
+        Normal = 0,
+        SemiTransparent = 1,
+        ObjWindow = 2,
+    }
+    
+    public struct Oam
+    {
+        // Packed Attributes
+        [FieldOffset(0x00)] public ushort Attribute0;
+        [FieldOffset(0x02)] public ushort Attribute1;
+        [FieldOffset(0x04)] public ushort Attribute2;
+        [FieldOffset(0x06)] public ushort? _padding;
+        
+        // Decoded from 0: SS D M OO P R YYYYYYYY
+        public OamShape Shape
+        {
+            get => (OamShape)((Attribute0 >> 14) & 0x03);
+            set => Attribute0 = (ushort)((Attribute0 & ~(0b11 << 14)) | ((ushort)value << 14));
+        }
+        public bool Use8BitsPerPixel
+        {
+            get => ((Attribute0 >> 13) & 0x01) != 0;
+            set => Attribute0 = (ushort)((Attribute0 & ~(1 << 13)) | ((value ? 1 : 0) << 13));
+        }
+        public bool IsMosaic
+        {
+            get => ((Attribute0 >> 12) & 0x01) != 0;
+            set => Attribute0 = (ushort)((Attribute0 & ~(1 << 12)) | ((value ? 1 : 0) << 12));
+        }
+        public OamMode Mode
+        {
+            get => (OamMode)((Attribute0 >> 10) & 0x03);
+            set => Attribute0 = (ushort)((Attribute0 & ~(0b11 << 10)) | ((ushort)value << 10));
+        }
+        public bool HiddenOrDoubleSize // Dependent on IsRotationScale
+        {
+            get => ((Attribute0 >> 9) & 0x01) != 0;
+            set => Attribute0 = (ushort)((Attribute0 & ~(1 << 9)) | ((value ? 1 : 0) << 9));
+        }
+        public bool IsRotationScale
+        {
+            get => ((Attribute0 >> 8) & 0x01) != 0;
+            set => Attribute0 = (ushort)((Attribute0 & ~(1 << 8)) | ((value ? 1 : 0) << 8));
+        }
+        public short Y
+        {
+            get
+            {
+                var rawY = Attribute0 & 0x00FFu;
+                return (short)((rawY >= 128) ? rawY - 256 : rawY);
+            }
+            set
+            {
+                ushort encoded = (ushort)(value < 0 ? value + 256 : value);
+                Attribute0 = (ushort)((Attribute0 & ~0x00FF) | (encoded & 0x00FF));
+            }
+        }
+
+        // Decoded from 1: SS PPPPP XXXXXXXXX
+        public byte SizeIndex
+        {
+            get => (byte)((Attribute1 >> 14) & 0x03);
+            set => Attribute1 = (ushort)((Attribute1 & ~(0b11 << 14)) | ((ushort)(value & 0x03) << 14));
+        }
+        public short RotationParamsOrFlipFlags // Dependent on IsRotationScale
+        {
+            get => (short)((Attribute1 >> 9) & 0x0F);
+            set => Attribute1 = (ushort)((Attribute1 & ~(0x0F << 9)) | ((ushort)(value & 0x0F) << 9));
+        }
+        public short X
+        {
+            get
+            {
+                uint rawX = Attribute1 & 0x01FFu;
+                return (short)((rawX >= 256) ? rawX - 512 : rawX);
+            }
+            set
+            {
+                ushort encoded = (ushort)(value < 0 ? value + 512 : value);
+                Attribute1 = (ushort)((Attribute1 & ~0x01FF) | (encoded & 0x01FF));
+            }
+        }
+        
+        // Decoded 2: IIII PP TTTTTTTTTT
+        public byte PaletteID
+        {
+            get => (byte)((Attribute2 >> 12) & 0x07);
+            set => Attribute2 = (ushort)((Attribute2 & ~(0b111 << 12)) | ((ushort)(value & 0x07) << 12));
+        }
+        public byte Priority
+        {
+            get => (byte)((Attribute2 >> 10) & 0x03);
+            set => Attribute2 = (ushort)((Attribute2 & ~(0b11 << 10)) | ((ushort)(value & 0x03) << 10));
+        }
+        public ushort TileIndex
+        {
+            get => (ushort)(Attribute2 & 0x03FF);
+            set => Attribute2 = (ushort)((Attribute2 & ~0x03FF) | (value & 0x03FF));
+        }
+        
+        // Helpers
+        public ushort Width =>
+            Shape switch
+            {
+                OamShape.Square =>     SizeIndex switch { 0 =>  8, 1 => 16, 2 => 32, 3 => 64, _ => 8 },
+                OamShape.Horizontal => SizeIndex switch { 0 => 16, 1 => 32, 2 => 32, 3 => 64, _ => 16 },
+                OamShape.Vertical =>   SizeIndex switch { 0 =>  8, 1 => 8, 2 => 16, 3 => 32, _ => 8 },
+                _ => 8,
+            };
+
+        public ushort Height =>
+            Shape switch
+            {
+                OamShape.Square =>     SizeIndex switch { 0 =>  8, 1 => 16, 2 => 32, 3 => 64, _ => 8 },
+                OamShape.Horizontal => SizeIndex switch { 0 =>  8, 1 => 8, 2 => 16, 3 => 32, _ => 8 },
+                OamShape.Vertical =>   SizeIndex switch { 0 => 16, 1 => 32, 2 => 32, 3 => 64, _ => 16 },
+                _ => 8,
+            };
+
+        public static Oam ReadOam(BinaryReader br, bool has8ByteStride = false)
+        {
+            var oam = new Oam{ Attribute0 = br.ReadUInt16(), Attribute1 = br.ReadUInt16(), Attribute2 = br.ReadUInt16() };
+            if (has8ByteStride) br.ReadUInt16();
+            return oam;
+        }
+    }
+}
