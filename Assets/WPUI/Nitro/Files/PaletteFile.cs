e4f49a2c2063dd370223c0bc87b82f4a7253ee19
Oli <hewisonoliver@gmail.com>
2025-08-13 00:31:42 +0100

    NCLR parsing (PaletteFile)

--- Assets/WPUI/Nitro/Files/PaletteFile.cs
+++ Assets/WPUI/Nitro/Files/PaletteFile.cs
@@ -1,19 +1,67 @@
 ﻿using System.IO;
+using System.Runtime.InteropServices;
 using WPUI.Nitro.Attributes;
+using WPUI.Nitro.Utils;
 
 namespace WPUI.Nitro.Files
 {
     [Magic("NCLR")]
     public sealed class PaletteFile : NitroFile
     {
+        [Magic("PLTT"), StructLayout(LayoutKind.Explicit)]
+        public struct PLTTBlock
+        {
+            [FieldOffset(0x00)] public ColorFormat ColorFormat; // 3 = 4bpp, 4=8bpp
+            [FieldOffset(0x04)] public uint ExtendedPalette;
+            [FieldOffset(0x08)] public uint PaletteDataSize;
+            [FieldOffset(0x0C)] public uint PaletteDataOffset; // Offset from TTLP+8 to Palette Data
+        }
+
+        public PLTTBlock plttBlock;
+        public Rgb8Color[] colors;
         public override void Read(BinaryReader br)
         {
-            throw new System.NotImplementedException();
+            ReadHeader(br);
+            for (int blockIdx = 0; blockIdx < header.BlockCount; blockIdx++)
+            {
+                long blockStart = br.BaseStream.Position;
+                string magic = ReadMagic(br);
+                int blockSize = (int)br.ReadUInt32();
+                switch (magic)
+                {
+                    case "TTLP":
+                    {
+                        plttBlock = ReadStruct<PLTTBlock>(br);
+                        br.BaseStream.Seek(blockStart + 8 + plttBlock.PaletteDataOffset, SeekOrigin.Current);
+                        colors = new Rgb8Color[plttBlock.PaletteDataSize / sizeof(uint)];
+                        for (int colorIdx = 0; colorIdx < colors.Length; colorIdx++)
+                        {
+                            colors[colorIdx] = ColorUtils.ReadRgb8FromBgr555(br);
+                        }
+                        break;
+                    }
+                    // TODO PCMP block for compressed colors
+                }
+                br.BaseStream.Seek(blockStart + blockSize, SeekOrigin.Current);
+            }
         }
 
         public override void Write(BinaryWriter bw)
         {
-            throw new System.NotImplementedException();
+            var plltSize = Marshal.SizeOf<PLTTBlock>();
+            plttBlock.PaletteDataSize = (uint)colors.Length * 2;
+            plttBlock.PaletteDataOffset = (uint)(bw.BaseStream.Position + plltSize);
+            header.BlockCount = 1;
+            header.FileSize = header.HeaderSize + 8u + (uint)plltSize + plttBlock.PaletteDataSize;
+            
+            WriteHeader(bw);
+            bw.Write(MagicAttribute.GetMagicBytes(typeof(PLTTBlock)));
+            bw.Write(8U + (uint)plltSize + plttBlock.PaletteDataSize);
+            WriteStruct(bw, plttBlock);
+            for (int i = 0; i < colors.Length; i++)
+            {
+                bw.Write(ColorUtils.Rgb8ToBgr555(colors[i]));
+            }
         }
     }
 }
---
+++ Assets/WPUI/Nitro/Utils/ColorUtils.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+
+namespace WPUI.Nitro.Utils
+{
+    public static class ColorUtils
+    {
+        public static Rgb8Color ReadRgb8FromBgr555(BinaryReader br) => Bgr555ToRgb8(br.ReadUInt16());
+        public static Rgb8Color Bgr555ToRgb8(ushort bgr)
+        {
+            int r = (bgr & 0x1F);
+            int g = (bgr >> 5) & 0x1F;
+            int b = (bgr >> 10) & 0x1F;
+
+            byte R = (byte)((r * 255 + 15) / 31);
+            byte G = (byte)((g * 255 + 15) / 31);
+            byte B = (byte)((b * 255 + 15) / 31);
+
+            return new Rgb8Color {R = R, G = G, B = B};
+        }
+
+        public static ushort Rgb8ToBgr555(Rgb8Color color)
+        {
+            // Convert 8-bit channels to 5-bit
+            ushort R = (ushort)(color.R >> 3); // 0–31
+            ushort G = (ushort)(color.G >> 3);
+            ushort B = (ushort)(color.B >> 3);
+
+            // Pack into BGR555 format: [0|RRRRR|GGGGG|BBBBB]
+            return (ushort)((R << 10) | (G << 5) | B);
+        }
+    }
+
+    public struct Rgb8Color
+    {
+        public byte R, G, B;
+    }
+
+    public enum ColorFormat : uint
+    {
+        _4BitsPerPixel = 3,
+        _8BitsPerPixel = 4,
+    }
+}
