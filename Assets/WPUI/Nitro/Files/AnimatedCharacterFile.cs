bf00b9f677a1f45ad90fd3d587d9b7c64cae072b
Oli <hewisonoliver@gmail.com>
2025-08-15 23:35:01 +0100

    AnimatedCharacterFile (NMCR) file parsing

--- Assets/WPUI/Nitro/Files/AnimatedCharacterFile.cs
+++ Assets/WPUI/Nitro/Files/AnimatedCharacterFile.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Runtime.InteropServices;
 using WPUI.Nitro.Attributes;
 
 namespace WPUI.Nitro.Files
@@ -6,9 +7,65 @@ namespace WPUI.Nitro.Files
     [Magic("NMCR")]
     public sealed class AnimatedCharacterFile : NitroFile
     {
+        [Magic("MCBK"), StructLayout(LayoutKind.Explicit, Size = 0x10)]
+        public struct MCBKHeader
+        {
+            [FieldOffset(0x00)] public ushort AnimationCount;
+            [FieldOffset(0x02)] public ushort _padding;
+            [FieldOffset(0x04)] public uint MappedAnimationDataOffset;
+            [FieldOffset(0x08)] public uint MappedAnimationCellDataOffset;
+            [FieldOffset(0x0C)] public ulong _padding2;
+        }
+
+        [StructLayout(LayoutKind.Explicit, Size = 0x08)]
+        public struct MappedAnimation
+        {
+            [FieldOffset(0x00)] public ushort CellCount;
+            [FieldOffset(0x02)] public ushort _unknown;
+            [FieldOffset(0x04)] public uint MappedAnimationCellDataOffset;
+
+            public MappedAnimationCell[] MappedAnimationCells;
+        }
+
+        [StructLayout(LayoutKind.Explicit, Size = 0x08)]
+        public struct MappedAnimationCell
+        {
+            [FieldOffset(0x00)] public ushort AnimationCellIndex;
+            [FieldOffset(0x02)] public short X;
+            [FieldOffset(0x04)] public short Y;
+            [FieldOffset(0x06)] public byte _unknown;
+            [FieldOffset(0x07)] public byte Priority;
+        }
+
+        public MCBKHeader mcbkHeader;
+        public MappedAnimation[] MappedAnimations;
+        
         public override void Read(BinaryReader br)
         {
-            throw new System.NotImplementedException();
+            ReadHeader(br);
+            for (int blockIdx = 0; blockIdx < header.BlockCount; blockIdx++)
+            {
+                long blockStart = br.BaseStream.Position;
+                string magic = ReadMagic(br);
+                int blockSize = (int)br.ReadUInt32();
+                switch (magic)
+                {
+                    case "KBCM":
+                    {
+                        mcbkHeader = ReadStruct<MCBKHeader>(br);
+                        br.BaseStream.Seek(blockStart + 8 + mcbkHeader.MappedAnimationDataOffset, SeekOrigin.Current);
+                        MappedAnimations = ReadStructArray<MappedAnimation>(br, mcbkHeader.AnimationCount);
+                        for (int animationIdx = 0; animationIdx < mcbkHeader.AnimationCount; animationIdx++)
+                        {
+                            br.BaseStream.Seek(blockStart + 8 + mcbkHeader.MappedAnimationCellDataOffset + MappedAnimations[animationIdx].MappedAnimationCellDataOffset, SeekOrigin.Current);
+                            MappedAnimations[animationIdx].MappedAnimationCells =
+                                ReadStructArray<MappedAnimationCell>(br, MappedAnimations[animationIdx].CellCount);
+                        }
+                        break;
+                    }
+                }
+                br.BaseStream.Seek(blockStart + blockSize, SeekOrigin.Current);
+            }
         }
 
         public override void Write(BinaryWriter bw)
